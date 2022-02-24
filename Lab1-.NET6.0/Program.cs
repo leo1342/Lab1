7d6e148cbd0d6bdc8a18eed778827d0275420962
leona <leona@DESKTOP-NQUMTG7>
2022-02-24 18:44:17 +0200

    Add project files.

---
+++ Lab1-.NET6.0/Program.cs
@@ -0,0 +1,9 @@
+﻿string name;
+Console.WriteLine("What is your name?");
+try 
+{ 
+    name = Console.ReadLine();
+    Console.WriteLine($"Hello {name}! Welcome to the .NET 6 World!");
+}
+catch(Exception ex) { Console.WriteLine("Eroare"); }
+
